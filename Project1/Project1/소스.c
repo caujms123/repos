38c2646c3afdbc9ca772af91e22ade47bb6e8050
caujms123 <zq3211@naver.com>
2022-12-16 16:21:58 +0900

    Update 소스.c

--- Project1/Project1/소스.c
+++ Project1/Project1/소스.c
@@ -4,7 +4,7 @@
 int main(void) {
 	long long N;
 	scanf("%lli", &N);
-	long long a = (float)3 / 2 * N*N + (float)5 / 2 * N + 1;
+	long long a = N*N;
 	long long result = a % 45678;
 	printf("%lli", result);
 	return 0;
